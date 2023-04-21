6be170a722cf233645d53504076826a6597bbe45
M4x <maxmurray1999@gmail.com>
2023-04-21 12:36:01 +0100

    Created Input Component. Value of text is saved in state and accessible from App component

---
+++ spellchecker/src/Components/App/index.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Input from '../Input/index.js'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Input onSubmit=""/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
